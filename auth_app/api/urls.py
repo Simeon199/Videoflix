35f138341dc5cb5341626e7e41f98f0c346109c2
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-03-09 20:31:01 +0100

    feat: LoginView and LogoutView added as well as the respective url routes

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RegistrationView, ActivationView
+from .views import RegistrationView, ActivationView, LoginView, LogoutView
 
 urlpatterns = [
     path('api/register/', RegistrationView.as_view(), name='registration'),
-    path('api/activate/<str:uidb64>/<str:token>/', ActivationView.as_view(), name='activate')
+    path('api/activate/<str:uidb64>/<str:token>/', ActivationView.as_view(), name='activate'),
+    path('api/login/', LoginView.as_view(), name='login'),
+    path('api/logout/', LogoutView.as_view(), name='logout')
 ]
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -5,8 +5,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
 
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, LoginSerializer
 from .utils import send_activation_email
 
 class RegistrationView(APIView):
@@ -52,3 +54,67 @@ class ActivationView(APIView):
             {'error': 'Aktivierung fehlgeschlagen.'},
             status=status.HTTP_400_BAD_REQUEST
         )
+    
+class LoginView(APIView):
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+
+        refresh = RefreshToken.for_user(user)
+
+        response = Response({
+            'detail': 'Login successful',
+            'user': {
+                'id': user.id,
+                'username': user.username
+            }
+        }, status=status.HTTP_200_OK)
+
+        response.set_cookie(
+            key='access_token',
+            value=str(refresh.access_token),
+            httponly=True,
+            secure=True,
+            samesite='None',
+        )
+
+        response.set_cookie(
+            key='refresh_token',
+            value=str(refresh),
+            httponly=True,
+            secure=True,
+            samesite='None'
+        )
+
+        return response
+
+class LogoutView(APIView):
+
+    def post(self, request):
+        refresh_token = request.COOKIES.get('refresh_token')
+
+        if not refresh_token:
+            return Response(
+                {'detail': 'Refresh-Token fehlt.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        except TokenError:
+            return Response(
+                {'detail': 'Token ist ungültig oder bereits abgelaufen.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        response = Response({
+            'detail': 'Logout successful! All tokens will be deleted. Refresh token is now invalid.'
+        }, status=status.HTTP_200_OK)
+
+        response.delete_cookie('access_token')
+        response.delete_cookie('refresh_token')
+
+        return response
