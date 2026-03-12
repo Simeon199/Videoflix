c12eb88be564c87ee02c9a7efc4d9f52676cff4b
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-03-12 20:05:48 +0100

    feat: send_password_reset_email method to utils.py file of auth_app added

--- auth_app/api/utils.py
+++ auth_app/api/utils.py
@@ -14,3 +14,19 @@ def send_activation_email(user, uidb64, token):
     )
     email = EmailMessage(subject, body, settings.DEFAULT_FROM_EMAIL, [user.email])
     email.send(fail_silently=False)
+
+def send_password_reset_email(user, uidb64, token):
+    reset_link = (
+        f"{settings.DOMAIN}/api/password_reset_confirm/{uidb64}/{token}/"
+    )
+    subject = 'Passwort zurücksetzen - Videoflix'
+    body = (
+        f"Hallo, \n\n"
+        f"du hast eine Anfrage zum Zurücksetzen deines Passworts gestellt.\n\n"
+        f"Klicke auf den folgenden Link, um ein neues Passwort zu vergeben:\n\n"
+        f"{reset_link}\n\n"
+        f"Falls du diese Anfrage nicht gestellt hast, kannst du diese Email ignorieren.\n\n"
+        f"Viele Grüße,\nDein Videoflix-Team"
+    )
+    email = EmailMessage(subject, body, settings.DEFAULT_FROM_EMAIL, [user.email])
+    email.send(fail_silently=False)
