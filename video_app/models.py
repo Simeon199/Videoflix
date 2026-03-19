26cd12e1a12ac0d92fdf5ca57090c76b07887372
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-03-19 19:19:11 +0100

    feat: VideoSerializer added to video_app

--- video_app/models.py
+++ video_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    CATEGORY_CHOICES = [
+        ("Drama", "Drama"),
+        ("Romance", "Romance"),
+        ("Comedy", "Comedy"),
+        ("Action", "Action"),
+        ("Documentary", "Documentary"),
+    ]
+
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to="thumbnail/")
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title 
