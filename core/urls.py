e829fc4a16b83ebd350e78c45385e39c157a77a4
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-03-19 19:26:25 +0100

    feat: urlpatterns updated in urls.py file of core directory

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('auth_app.api.urls'))
+    path('', include('auth_app.api.urls')),
+    path('', include('video_app.api.urls')),
 ]
