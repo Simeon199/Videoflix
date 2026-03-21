dcadcaebb4bef6dc115620bc25db4d7c01f1afb5
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-03-21 16:12:46 +0100

    feat: HLSManifestView added to views.py file of the video_app

--- video_app/api/views.py
+++ video_app/api/views.py
@@ -1,5 +1,10 @@
+import os
+from django.conf import settings
+from django.core.cache import cache
+from django.http import HttpResponse, Http404
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 from auth_app.api.authentication import CookieJWTAuthentication
 from video_app.models import Video
 from video_app.api.serializers import VideoSerializer
@@ -9,3 +14,27 @@ class VideoListView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = VideoSerializer
     queryset = Video.objects.all().order_by("-created_at")
+
+class HLSManifestView(APIView):
+    authentication_classes = [CookieJWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, movie_id, resolution):
+        cache_key = f"hls_manifest_{movie_id}_{resolution}"
+        cached_content = cache.get(cache_key)
+
+        if cached_content:
+            return HttpResponse(cached_content, content_type="application/vnd.apple.mpegurl")
+        
+        file_path = os.path.join(
+            settings.MEDIA_ROOT, "videos", str(movie_id), resolution, "index.m3u8"
+        )
+
+        if not os.path.exists(file_path):
+            raise Http404("Video nicht gedunden.")
+        
+        with open(file_path, "r") as f:
+            content = f.read()
+
+        cache.set(cache_key, content, timeout=300)
+        return HttpResponse(content, content_type="application/vnd.apple.mpegurl")
