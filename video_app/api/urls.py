4ae0bad91608875244c3135fa873dcb9f3a9ab1c
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-03-21 16:23:02 +0100

    feat: urls for respective movie_ids added to the urls.py file of the video_app

--- video_app/api/urls.py
+++ video_app/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from video_app.api.views import VideoListView
+from video_app.api.views import VideoListView, HLSManifestView, HLSSegmentView
 
 urlpatterns = [
     path("api/video/", VideoListView.as_view(), name="video-list"),
+    path("api/video/<int:movie_id>/<str:resolution>/index.m3u8", HLSManifestView.as_view(), name="hls-manifest"),
+    path("api/video/<int:movie_id>/<str:resolution>/<str:segment>/", HLSSegmentView.as_view(), name="hls-segment")
 ]
