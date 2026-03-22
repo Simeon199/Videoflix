9765b2a1385788c3dbc473b5f3008fde2ae1480d
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-03-22 21:18:04 +0100

    feat: setup changes including docker-compose.yml and backend.entrypoint.sh to make video upload possible on local device for testing purposes

--- backend.entrypoint.sh
+++ backend.entrypoint.sh
@@ -38,6 +38,8 @@ else:
     print(f"Superuser '{username}' already exists.")
 EOF
 
-python manage.py rqworker default &
+exec "$@"
 
-exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --reload
+# python manage.py rqworker default &
+
+# exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --reload
